3c0989cbedec6e6e595abb9ded8c5a0433ad29ec
David Medin <david@davidmedin.com>
2023-09-27 13:10:17 -0500

    Can kill entities. What does the brain do now?

--- src/breakable.zig
+++ src/breakable.zig
@@ -1,22 +1,21 @@
-const context = @import("context.zig");
+const std = @import("std");
 const ecs = @import("ecs");
+const context = @import("context.zig");
 
-pub const Breakable = struct {
-	max_health : u64,
-	health : u64
-};
+pub const Breakable = struct { max_health: u64, health: u64 };
 
-pub fn take_damage(ctx : *context.Context, me : ecs.Entity) !void {
-    var mes_breakable : *Breakable = (try ctx.*.world.get_component(me, "breakable", Breakable)).?;
-	
-	if(mes_breakable.*.health != 0) {
-		mes_breakable.*.health -= 1;
-	}
+pub fn take_damage(ctx: *context.Context, me: ecs.Entity) !void {
+    var mes_breakable: *Breakable = (try ctx.*.world.get_component(me, "breakable", Breakable)).?;
 
-	if(mes_breakable.*.health == 0){
-		// ctx.*.world.
-		// Queue deletion
-	}
+    if (mes_breakable.*.health != 0) {
+        mes_breakable.*.health -= 1;
+    }
+
+    //									  v --- plot armor
+    if (mes_breakable.*.health == 0 and !std.meta.eql(me, ctx.*.player_entity)) {
+        try ctx.*.world.queue_kill_entity(me);
+        // Queue deletion
+    }
 }
 
 // pub fn take_damage(ctx : *context.Context, self : *Breakable, self_entity : ecs.Entity, damage : u32) void {
--- src/main.zig
+++ src/main.zig
@@ -34,7 +34,6 @@ const breakable = @import("breakable.zig");
 // [] GUI ECS viewer/editor in and/or out of the playdate. HARD
 // [] Traceing. Use trace.zig - need to make frakenstein to use playdate api instead of std.os
 
-
 // ===============================================================
 // Don't use these unless your logFn (you're not).
 var GLOBAL_PLAYDATE: ?*pdapi.PlaydateAPI = null;
@@ -64,7 +63,7 @@ fn component_allocator(playdate_api: *pdapi.PlaydateAPI) std.mem.Allocator {
         .vtable = &std.mem.Allocator.VTable{
             .alloc = struct {
                 pub fn alloc(ctx: *anyopaque, len: usize, ptr_align: u8, ret_addr: usize) ?[*]u8 {
-                    if(ALLOC_PRINT) {
+                    if (ALLOC_PRINT) {
                         GLOBAL_PLAYDATE.?.*.system.logToConsole("Alloc : len : %p, ptr_align : %d, ret_addr : %p\n", len, ptr_align, ret_addr);
                     }
                     const playdate: *pdapi.PlaydateAPI = @ptrCast(@alignCast(ctx));
@@ -76,8 +75,8 @@ fn component_allocator(playdate_api: *pdapi.PlaydateAPI) std.mem.Allocator {
             .resize = struct {
                 pub fn resize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
                     _ = ctx;
-                    if(ALLOC_PRINT) {
-                        GLOBAL_PLAYDATE.?.*.system.logToConsole("Resize : buff_ptr : %p, buff_len : %d, new_len : %d, ret_addr : %p\n", buf.ptr,buf.len,buf_align,new_len,ret_addr);
+                    if (ALLOC_PRINT) {
+                        GLOBAL_PLAYDATE.?.*.system.logToConsole("Resize : buff_ptr : %p, buff_len : %d, new_len : %d, ret_addr : %p\n", buf.ptr, buf.len, buf_align, new_len, ret_addr);
                     }
                     return false; // Playdate doesn't have a function to *attempt* to resize without moving. It will just allocate it.
                     // So, lets hope that every user of .resize will have a fallback 'free' and 'alloc'.
@@ -85,8 +84,8 @@ fn component_allocator(playdate_api: *pdapi.PlaydateAPI) std.mem.Allocator {
             }.resize,
             .free = struct {
                 pub fn free(ctx: *anyopaque, buf: []u8, buf_align: u8, ret_addr: usize) void {
-                    if(ALLOC_PRINT) {
-                        GLOBAL_PLAYDATE.?.*.system.logToConsole("Free : buff_ptr : %p, buff_len : %d, ret_addr : %p\n", buf.ptr,buf.len,buf_align,ret_addr);
+                    if (ALLOC_PRINT) {
+                        GLOBAL_PLAYDATE.?.*.system.logToConsole("Free : buff_ptr : %p, buff_len : %d, ret_addr : %p\n", buf.ptr, buf.len, buf_align, ret_addr);
                     }
                     const playdate: *pdapi.PlaydateAPI = @ptrCast(@alignCast(ctx));
                     _ = playdate.system.realloc(buf.ptr, 0);
@@ -107,20 +106,20 @@ pub export fn eventHandler(playdate: *pdapi.PlaydateAPI, event: pdapi.PDSystemEv
             GLOBAL_ALLOCATOR = component_allocator(playdate);
 
             const ecs_config = ecs.ECSConfig{ .component_allocator = GLOBAL_ALLOCATOR.? };
-            var ctx : *context.Context = GLOBAL_ALLOCATOR.?.create(context.Context) catch unreachable; // allocate a context. This will never be free'd
-            ctx.* =  context.Context{
+            var ctx: *context.Context = GLOBAL_ALLOCATOR.?.create(context.Context) catch unreachable; // allocate a context. This will never be free'd
+            ctx.* = context.Context{
                 .playdate = playdate,
                 .allocator = component_allocator(playdate),
                 .world = ecs.ECS.init(ecs_config) catch unreachable,
                 .map = map.Map.init(ctx), // Kinda sus, but does work. As long as map.init does't assume .tileset has been written to...
-                .tileset = playdate.graphics.loadBitmapTable("tilemap", null).?
+                .tileset = playdate.graphics.loadBitmapTable("tilemap", null).?,
             };
             // g_playdate_image = playdate.graphics.loadBitmap("playdate_image", null).?;
             const font = playdate.graphics.loadFont("/System/Fonts/Asheville-Sans-14-Bold.pft", null).?;
             playdate.graphics.setFont(font);
 
             init(ctx) catch unreachable;
-            
+
             playdate.system.setUpdateCallback(update, ctx);
         },
         .EventTerminate => {
@@ -131,15 +130,15 @@ pub export fn eventHandler(playdate: *pdapi.PlaydateAPI, event: pdapi.PDSystemEv
     return 0;
 }
 
-fn init(ctx : *context.Context) !void {
+fn init(ctx: *context.Context) !void {
     const playdate = ctx.*.playdate;
     const world = &ctx.*.world;
 
     // ECS things
-    const hero_image = playdate.graphics.getTableBitmap(ctx.*.tileset,4).?; // Index 4 is the person sprite
-    const goblin_image = playdate.graphics.getTableBitmap(ctx.*.tileset,5).?; // Index 5 is the goblin sprite
+    const hero_image = playdate.graphics.getTableBitmap(ctx.*.tileset, 4).?; // Index 4 is the person sprite
+    const goblin_image = playdate.graphics.getTableBitmap(ctx.*.tileset, 5).?; // Index 5 is the goblin sprite
 
-    {    // Brain entity
+    { // Brain entity
         const player_brain: ecs.Entity = try world.new_entity();
         try world.add_component(player_brain, "brain", brain.Brain{ .reaction_time = 0, .body = undefined });
         try world.add_component(player_brain, "controls", controls.Controls{ .movement = 0 });
@@ -153,11 +152,11 @@ fn init(ctx : *context.Context) !void {
 
         try world.add_component(player_body, "image", image.Image{ .bitmap = hero_image });
         try world.add_component(player_body, "transform", transform.Transform{ .x = 4, .y = 4 });
-        try world.add_component(player_body, "relation", ai.Relation{.in= ai.Relation.HUMAN, .hates = 0, .loves = 0});
-        try world.add_component(player_body, "breakable", breakable.Breakable{.max_health = 4, .health = 4});
+        try world.add_component(player_body, "relation", ai.Relation{ .in = ai.Relation.HUMAN, .hates = 0, .loves = 0 });
+        try world.add_component(player_body, "breakable", breakable.Breakable{ .max_health = 4, .health = 4 });
     }
 
-    {    // Brain entity
+    { // Brain entity
         const enemy_brain: ecs.Entity = try world.new_entity();
         try world.add_component(enemy_brain, "brain", brain.Brain{ .reaction_time = 1, .body = undefined });
         try world.add_component(enemy_brain, "ai", ai.AI{});
@@ -170,15 +169,14 @@ fn init(ctx : *context.Context) !void {
 
         try world.add_component(enemy_body, "image", image.Image{ .bitmap = goblin_image });
         try world.add_component(enemy_body, "transform", transform.Transform{ .x = 4, .y = 6 });
-        try world.add_component(enemy_body, "relation", ai.Relation{.in= ai.Relation.GOBLIN, .hates = ai.Relation.HUMAN, .loves = 0});
-        try world.add_component(enemy_body, "breakable", breakable.Breakable{.max_health = 4, .health = 4});
-
+        try world.add_component(enemy_body, "relation", ai.Relation{ .in = ai.Relation.GOBLIN, .hates = ai.Relation.HUMAN, .loves = 0 });
+        try world.add_component(enemy_body, "breakable", breakable.Breakable{ .max_health = 4, .health = 4 });
     }
     // Time stuff
     playdate.system.resetElapsedTime();
     tick(ctx) catch unreachable;
 
-    std.log.debug("Finished the init!",.{});
+    std.log.debug("Finished the init!", .{});
 }
 
 var time_leftovers: f32 = 0;
@@ -186,7 +184,6 @@ fn update(userdata: ?*anyopaque) callconv(.C) c_int {
     const ctx: *context.Context = @ptrCast(@alignCast(userdata.?));
     const playdate = ctx.*.playdate;
     const world = &ctx.*.world;
-
 
     const tick_length: f32 = 0.25; // In seconds (microsecond accuracy)
     const time: f32 = playdate.system.getElapsedTime();
@@ -218,13 +215,12 @@ fn update(userdata: ?*anyopaque) callconv(.C) c_int {
     }
 
     {
-        const player_breakable : *breakable.Breakable = (world.get_component(ctx.*.player_entity, "breakable", breakable.Breakable) catch unreachable).?;
-        const args = .{player_breakable.*.max_health,player_breakable.*.health};
-        var buffer : [100]u8 = undefined; // Using a buffered format because it is faster. Also, Health : {} is never bigger than 100 characters.
-        const buffer_slice : []u8 = std.fmt.bufPrint(buffer[0..], "Health : {}/{}", args) catch unreachable; // A slice into 'buffer'.
+        const player_breakable: *breakable.Breakable = (world.get_component(ctx.*.player_entity, "breakable", breakable.Breakable) catch unreachable).?;
+        const args = .{ player_breakable.*.max_health, player_breakable.*.health };
+        var buffer: [100]u8 = undefined; // Using a buffered format because it is faster. Also, Health : {} is never bigger than 100 characters.
+        const buffer_slice: []u8 = std.fmt.bufPrint(buffer[0..], "Health : {}/{}", args) catch unreachable; // A slice into 'buffer'.
         const width = playdate.graphics.drawText(buffer_slice.ptr, buffer_slice.len, pdapi.PDStringEncoding.ASCIIEncoding, 2, 240 - 16);
         _ = width;
-
     }
 
     // returning 1 signals to the OS to draw the frame.
@@ -234,19 +230,18 @@ fn update(userdata: ?*anyopaque) callconv(.C) c_int {
 
 fn tick(ctx: *context.Context) !void {
     {
-        var iter = ecs.data_iter(.{.brain = brain.Brain}).init(&ctx.*.world);
-        while(iter.next()) |slice| {
+        var iter = ecs.data_iter(.{ .brain = brain.Brain }).init(&ctx.*.world);
+        while (iter.next()) |slice| {
             brain.react(slice.brain);
         }
     }
 
     {
-        var iter = ecs.data_iter(.{.ai = ai.AI, .brain = brain.Brain}).init(&ctx.*.world);
-        while(iter.next()) |slice| {
-            try ai.move(ctx, slice.ai,slice.brain);
+        var iter = ecs.data_iter(.{ .ai = ai.AI, .brain = brain.Brain }).init(&ctx.*.world);
+        while (iter.next()) |slice| {
+            try ai.move(ctx, slice.ai, slice.brain);
         }
     }
-
 
     {
         var move_iter = ecs.data_iter(.{ .controls = controls.Controls, .brain = brain.Brain }).init(&ctx.*.world);
@@ -255,5 +250,6 @@ fn tick(ctx: *context.Context) !void {
         }
     }
 
-
+    try ctx.*.world.kill_queued_entities();
+    try ctx.*.world.remove_queued_components();
 }
